3e416fabaa6a70f2450852a386c2a21183d221a2
WalakCode <gilhansen96@gmail.com>
2023-10-27 17:59:05 -0500

    seleccionado id

--- model/userModel.js
+++ model/userModel.js
@@ -14,16 +14,32 @@ const userModel={
         })
         // dbC.closeConn()
     },
+    maxId:()=>{
+        const query = 'SELECT MAX(ID)+1 AS id FROM users'
+        const conn = dbC.getConn()
+        conn.query(query,(err,result)=>{
+            if(err){
+                return null
+            }
+            if(result){
+                const Id = result[0].id
+                console.log(Id)
+                return Id
+            }
+        })
+    },
+
     createUser:(userinfo,callback)=>{
         const members = parseInt(userinfo.members) 
         const gender = parseInt(userinfo.gender)
 
-        const userinfoArray = [userinfo.username,userinfo.name,members,userinfo.birthday,userinfo.password,gender]
-        const query = 'INSERT INTO users (Username, FullName, FamilyCount, BirthDate, Password, Gender) VALUES (?, ?, ?, ?, ?, ?)';
-        const conn = dbC.getConn()
+        const Id = userModel.maxId();
 
+        const userinfoArray = [Id,2,userinfo.username,userinfo.password,userinfo.name,gender,userinfo.birthday,members]
         console.log(userinfoArray)
 
+        const query = 'INSERT INTO users (ID,UserTypeID,Username,Password,FullName,Gender,BirthDate,FamilyCount) VALUES (?, ?, ?, ?, ?, ?)';
+        const conn = dbC.getConn()
         conn.query(query,userinfoArray,(err,results)=>{
             if(err){
                 console.log(err)
